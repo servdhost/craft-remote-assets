761b582b2a76bfb1b91cb32a5fc2f765ab728898
Matt <matt@bitbreakfast.co.uk>
2018-07-26 11:28:05 +0100

    Updated version

--- src/CraftRemoteAssets.php
+++ src/CraftRemoteAssets.php
@@ -36,7 +36,7 @@ class CraftRemoteAssets extends Plugin
 {
 
     public static $plugin;
-    public $schemaVersion = '0.1.5';
+    public $schemaVersion = '0.1.6';
 
     public function init()
     {
